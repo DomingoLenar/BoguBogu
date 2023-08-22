bf0e56cd9d8e2b2e4f4e531f329810f814e47f0d
Mark Lestat Agustin <2233915@slu.edu.ph>
2023-08-22 10:26:23 +0800

    +sorting algorithms

--- PrelimProj1/src/SortingAlgorithms.java
+++ PrelimProj1/src/SortingAlgorithms.java
@@ -1,2 +1,17 @@
 public class SortingAlgorithms {
+    public void selectionSort(String[] array){
+        int length = array.length;
+        for(int x = 0; x < length - 1; x++){
+            int minIndex = x;
+            for(int y = x + 1; y < length; y++){
+                if(array[minIndex].compareTo(array[y])>0) minIndex = y;
+            }
+            if(minIndex != x){
+                String temp = array[x];
+                array[x] = array[minIndex];
+                array[minIndex] = temp;
+            }
+        }
+
+    }
 }
