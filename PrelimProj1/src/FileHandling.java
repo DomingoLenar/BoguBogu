44569b427e0c57aca0066d15d48255fe436216e1
Mark Lestat Agustin <2233915@slu.edu.ph>
2023-08-22 10:26:06 +0800

    +file handling initial code

--- PrelimProj1/src/FileHandling.java
+++ PrelimProj1/src/FileHandling.java
@@ -1,2 +1,21 @@
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.util.Scanner;
 public class FileHandling {
+    public String[] fileRead(String fileName) throws FileNotFoundException{
+        File file = new File(fileName);
+        Scanner reader = new Scanner(file);
+
+    }
+
+    public int countLines(String fileName)throws FileNotFoundException{
+        Scanner counter = new Scanner(fileName);
+        int count = 0;
+        while(counter.hasNext()){
+           counter.nextLine();
+           count++;
+        }
+        counter.close();
+        return count;
+    }
 }
