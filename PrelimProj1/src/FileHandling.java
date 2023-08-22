7535f4a5216c18985f6cd7a827dd9c2f80c97ee7
Mark Lestat  Agustin <2233915@slu.edu.ph>
2023-08-22 21:06:03 +0800

    +partially complete fileRead method

--- PrelimProj1/src/FileHandling.java
+++ PrelimProj1/src/FileHandling.java
@@ -5,11 +5,16 @@ public class FileHandling {
     public String[] fileRead(String fileName) throws FileNotFoundException{
         File file = new File(fileName);
         Scanner reader = new Scanner(file);
-
+        int size = countLines(file);
+        String[] data = new String[size];
+        for(int x = 0; x < size; x++){
+            data[x] = reader.nextLine();
+        }
+        return data;
     }
 
-    public int countLines(String fileName)throws FileNotFoundException{
-        Scanner counter = new Scanner(fileName);
+    public int countLines(File file)throws FileNotFoundException{
+        Scanner counter = new Scanner(file);
         int count = 0;
         while(counter.hasNext()){
            counter.nextLine();
