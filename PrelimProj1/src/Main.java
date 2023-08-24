43bd862f51a6ba831d5a7a6a25e021e12f159edc
Lestat <2233915@slu.edu.ph>
2023-08-24 21:44:58 +0800

    +method to process files given using FileHandler class

--- PrelimProj1/src/Main.java
+++ PrelimProj1/src/Main.java
@@ -1,2 +1,37 @@
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Main {
+    FileHandling fileHandler = new FileHandling();
+    SortingAlgorithms sort = new SortingAlgorithms();
+    Scanner kInput = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Main program = new Main();
+        program.run();
+    }
+
+    public void run(){
+        ArrayList<String[]> data = processFile();
+    }
+
+    public ArrayList<String[]> processFile(){
+        boolean valid = true;
+        ArrayList<String[]> data = new ArrayList<>();
+        do {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter file name: ");
+            String fileName = input.nextLine();
+            try {
+                fileHandler.fileRead(fileName, data);
+                valid = true;
+            }catch(FileNotFoundException fileException){
+                valid = false;
+            }
+        }while(!valid);
+        return data;
+    }
+
+
+
 }
