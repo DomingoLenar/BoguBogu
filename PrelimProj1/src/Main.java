99a3a1be6d86a9bfda50adfaa3d96ba1d58cd02c
Lou Diamond Morados <2233672@slu.edu.ph>
2023-08-25 10:17:24 +0800

    +added insertion algo & user-interactivity

--- PrelimProj1/src/Main.java
+++ PrelimProj1/src/Main.java
@@ -12,12 +12,31 @@ public class Main {
     }
 
     public void run(){
+        int columnIndex = 0;
+        int sortingType = 0;
 
         ArrayList<String[]> data = processFile();
+        String[] toSearch = new String[data.size()];
+        promptMessage(columnIndex, sortingType);
+        populateSubArray(data, toSearch, 2);
+
+    }
+
+    private void promptMessage(int columnIndex, int sortingType) {
+        do {
+            System.out.println("Enter the column to be sort");
+            columnIndex =   kInput.nextInt();
+            System.out.println("Enter the sorting algorithm to be apply in data: 1. Bubble Sort | 2. Selection Sort | 3. Insertion Sort");
+            sortingType = kInput.nextInt();
+
+            if (columnIndex < 0); // add another layer of verification
+        } while (false); // loop if not valid otherwise break
+
+
     }
 
     public ArrayList<String[]> processFile(){
-        boolean valid = true;
+        boolean valid;
         ArrayList<String[]> data = new ArrayList<>();
         do {
             System.out.print("Enter file name: ");
@@ -32,10 +51,18 @@ public class Main {
         return data;
     }
 
-    public void printArray(ArrayList array){
+    public void printArray(String[] array){
         for (Object o : array) {
             System.out.println(o);
         }
     }
+
+    public void populateSubArray(ArrayList<String[]> arrayList, String[] arrayToPopulate, int columnIndex){
+        for(int x = 0; x < arrayList.size(); x++){
+            arrayToPopulate[x] = arrayList.get(x)[columnIndex];
+        }
+    }
+
+
 
 }
--- PrelimProj1/src/SortingAlgorithms.java
+++ PrelimProj1/src/SortingAlgorithms.java
@@ -28,8 +28,18 @@ public class SortingAlgorithms {
         }
     }
 
-    public void insertionSort(String[] arr)
+    public void insertionSort(String[] array)
     {
-        //do smth..
+        int length = array.length;
+        for (int i = 1; i < length; ++i) {
+            String key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j].compareTo(key) > 0) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
     }
 }
