c6715248afd61c0f11388880cc7588f41913af9c
Gebreyl Isaac Rabang <2233944@slu.edu.ph>
2023-08-25 10:13:44 +0800

    Added documentation comments, optimized import statements

--- PrelimProj1/src/Main.java
+++ PrelimProj1/src/Main.java
@@ -1,5 +1,4 @@
 import java.io.FileNotFoundException;
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Main {
@@ -10,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         Main program = new Main();
         program.run();
-    }
+    }//End of main method
 
     public void run(){
 
@@ -18,8 +17,12 @@ public class Main {
         int verticalSize = data.size();
         String[] toSearch = new String[verticalSize];
         populateArray(data, toSearch, 2);
-    }
+    }//End of run method
 
+    /**
+     * Method for checking the file with exception handling
+     * @return data
+     */
     public ArrayList<String[]> processFile(){
         boolean valid = true;
         ArrayList<String[]> data = new ArrayList<>();
@@ -35,21 +38,30 @@ public class Main {
             }
         }while(!valid);
         return data;
-    }
+    }//End of processFile method
 
+    /**
+     * Method for printing an array
+     * @param array
+     */
     public void printArray(ArrayList array){
         for (Object o : array) {
             System.out.println(o);
         }
-    }
+    }//End of printArray method
 
-
+    /**
+     * Method for populating an array
+     * @param arrayList
+     * @param arrayToPopu
+     * @param horizontalIndex
+     */
     public void populateArray(ArrayList<String[]> arrayList, String[] arrayToPopu, int horizontalIndex){
         for(int x = 0; x < arrayList.size(); x++){
             arrayToPopu[x] = arrayList.get(x)[horizontalIndex];
         }
-    }
+    }//End of populateArray method
 
 
 
-}
+}//End of main class
