d1abf3c3477de621a6ecf47f18496d011e689f88
Rizza <2234110@slu.edu.ph>
2023-08-25 09:54:43 +0800

    comments for filehandling

--- PrelimProj1/src/FileHandling.java
+++ PrelimProj1/src/FileHandling.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.util.Scanner;
 public class FileHandling {
     public String[] fileRead(String fileName) throws FileNotFoundException{
-        File file = new File(fileName);
-        Scanner reader = new Scanner(file);
-        int size = countLines(file);
-        String[] data = new String[size];
-        for(int x = 0; x < size; x++){
+        File file = new File(fileName); //creating a file
+        Scanner reader = new Scanner(file); //initializing a scanner to read a data from file
+        int size = countLines(file); // Count the number of lines in the file using the countLines method.
+        String[] data = new String[size]; // Create a string to store the read data, with the size calculated earlier.
+        for(int x = 0; x < size; x++){  // Loop through each line of the file and store it in the data array.
             data[x] = reader.nextLine();
         }
         return data;
