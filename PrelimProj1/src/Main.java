9796b869f3f3b0e49aaa02af35b786ac5a0b6268
Lou <2233672@slu.edu.ph>
2023-08-26 00:04:47 +0800

    +completed user-interactivity, added program flexibility, and conventions

--- PrelimProj1/src/Main.java
+++ PrelimProj1/src/Main.java
@@ -1,41 +1,59 @@
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
-public class Main {
+public class Main extends SortingAlgorithms implements Runnable {
     FileHandling fileHandler = new FileHandling();
-    SortingAlgorithms sort = new SortingAlgorithms();
     Scanner kInput = new Scanner(System.in);
+    ArrayList<String[]> data = new ArrayList<>();
+    String[] toSearch;
+
 
     public static void main(String[] args) {
         Main program = new Main();
         program.run();
     }
 
-    public void run(){
-        int columnIndex = 0;
-        int sortingType = 0;
+    @Override
+    public void run() {
 
-        ArrayList<String[]> data = processFile();
-        String[] toSearch = new String[data.size()];
-        promptMessage(columnIndex, sortingType);
-        populateSubArray(data, toSearch, 2);
+        data = processFile(); // data is dynamic
+        int rows_data =  data.size();
+        toSearch = new String[rows_data - 1];
+        promptMessage();
+        printArray(toSearch);
 
     }
 
-    private void promptMessage(int columnIndex, int sortingType) {
+    private void promptMessage() { // method binding (dynamic)
+        System.out.println("Below are the categories for each datum in a data set");
+        String[] arr_header = data.get(0); // header
+        data.remove(0); // remove header in data set
+        for(int i = 0; i < arr_header.length; i++)
+        {
+            System.out.print(i + 1 + "." + arr_header[i] + "\t");
+
+        }
+
+        int category_number;
+        int sortingType;
         do {
-            System.out.println("Enter the column to be sort");
-            columnIndex =   kInput.nextInt();
+            System.out.println("\nPlease input the category number to be sort: ");
+            category_number = kInput.nextInt();
             System.out.println("Enter the sorting algorithm to be apply in data: 1. Bubble Sort | 2. Selection Sort | 3. Insertion Sort");
             sortingType = kInput.nextInt();
 
-            if (columnIndex < 0); // add another layer of verification
-        } while (false); // loop if not valid otherwise break
+            if (!(category_number > 0  && category_number <= arr_header.length) || !(sortingType > 0  && sortingType < 4))
+            {
+                System.out.println("Please try again.");
+            }
+        } while (!(category_number > 0  && category_number <= arr_header.length) || !(sortingType > 0  && sortingType < 4));
 
+        populateSubArray(data, toSearch, category_number - 1);
+        sortingProcess(sortingType, toSearch);
 
     }
 
-    public ArrayList<String[]> processFile(){
+    private ArrayList<String[]> processFile(){
         boolean valid;
         ArrayList<String[]> data = new ArrayList<>();
         do {
@@ -51,18 +69,16 @@ public class Main {
         return data;
     }
 
-    public void printArray(String[] array){
+    private void printArray(String[] array){
         for (Object o : array) {
             System.out.println(o);
         }
     }
 
-    public void populateSubArray(ArrayList<String[]> arrayList, String[] arrayToPopulate, int columnIndex){
+    private void populateSubArray(ArrayList<String[]> arrayList, String[] arrayToPopulate, int columnIndex){
         for(int x = 0; x < arrayList.size(); x++){
             arrayToPopulate[x] = arrayList.get(x)[columnIndex];
         }
     }
-
-
 
 }
--- PrelimProj1/src/SortingAlgorithms.java
+++ PrelimProj1/src/SortingAlgorithms.java
@@ -1,45 +1,47 @@
 public class SortingAlgorithms {
-    public void selectionSort(String[] array){
-        int length = array.length;
-        for(int x = 0; x < length - 1; x++){
-            int minIndex = x;
-            for(int y = x + 1; y < length; y++){
-                if(array[minIndex].compareTo(array[y])>0) minIndex = y;
-            }
-            if(minIndex != x){
-                String temp = array[x];
-                array[x] = array[minIndex];
-                array[minIndex] = temp;
-            }
-        }
-
-    }
-
-    public void bubbleSort(String[] array){
-        int length = array.length;
-        for(int x = 0; x < length - 1; x++){
-            for(int y = x + 1; y < length; y++){
-                if(array[x].compareTo(array[y]) > 0){
-                    String temp = array[x];
-                    array[x] = array[y];
-                    array[y] = temp;
-                }
-            }
-        }
-    }
-
-    public void insertionSort(String[] array)
+    void sortingProcess(int sortingType, String[] array)
     {
         int length = array.length;
-        for (int i = 1; i < length; ++i) {
-            String key = array[i];
-            int j = i - 1;
 
-            while (j >= 0 && array[j].compareTo(key) > 0) {
-                array[j + 1] = array[j];
-                j = j - 1;
-            }
-            array[j + 1] = key;
+        switch (sortingType)
+        {
+            case 1: // selection sort
+                for (int x = 0; x < length - 1; x++) {
+                    int minIndex = x;
+                    for (int y = x + 1; y < length; y++) {
+                        if (array[minIndex].compareTo(array[y]) > 0) minIndex = y;
+                    }
+                    if (minIndex != x) {
+                        String temp = array[x];
+                        array[x] = array[minIndex];
+                        array[minIndex] = temp;
+                    }
+                }
+
+            case 2: // bubble sort
+                for (int x = 0; x < length - 1; x++) {
+                    for (int y = x + 1; y < length; y++) {
+                        if (array[x].compareTo(array[y]) > 0) {
+                            String temp = array[x];
+                            array[x] = array[y];
+                            array[y] = temp;
+                        }
+                    }
+                }
+
+            case 3: // insertion sort
+                for (int i = 1; i < length; ++i) {
+                    String key = array[i];
+                    int j = i - 1;
+
+                    while (j >= 0 && array[j].compareTo(key) > 0) {
+                        array[j + 1] = array[j];
+                        j = j - 1;
+                    }
+                    array[j + 1] = key;
+                }
         }
+
+
     }
 }
