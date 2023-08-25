0e9ce07ee7bf3c47e6a51c60a054107832b04fab
2233085 <2233085@slu.edu.ph>
2023-08-25 10:15:21 +0800

    Added partial documentation for the bubbleSort method in SortingAlgorithms.java

--- PrelimProj1/src/SortingAlgorithms.java
+++ PrelimProj1/src/SortingAlgorithms.java
@@ -15,16 +15,27 @@ public class SortingAlgorithms {
 
     }
 
+    // Sorts an array of strings using the bubble sort algorithm
     public void bubbleSort(String[] array){
-        int length = array.length;
+
+        int length = array.length; // Initializes the length of the array
+
         for(int x = 0; x < length - 1; x++){
             for(int y = x + 1; y < length; y++){
+
+                // Compares the elements in the array indexed in x and y
                 if(array[x].compareTo(array[y]) > 0){
+
+                    // Swaps the array's element in index x with the element in index y
                     String temp = array[x];
                     array[x] = array[y];
                     array[y] = temp;
-                }
-            }
-        }
-    }
+
+                } // End of if statement
+
+            } // End of second for loop
+
+        } // End of first for loop
+
+    } // End of bubbleSort method
 }
