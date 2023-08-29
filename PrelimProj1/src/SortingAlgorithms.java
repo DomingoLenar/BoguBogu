84b2b54830e023a203575c3705a66eb743faa273
Carlo Leeon Villareal <2233085@slu.edu.ph>
2023-08-29 10:29:19 +0800

    + Added comments on modified sorting algorithms.

--- PrelimProj1/src/SortingAlgorithms.java
+++ PrelimProj1/src/SortingAlgorithms.java
@@ -18,16 +18,21 @@ public class SortingAlgorithms {
                     }
                 }
 
-            case 2: // bubble sort
+            case 2:  // Sorts an array of strings using the bubble sort algorithm
                 for (int x = 0; x < length - 1; x++) {
                     for (int y = x + 1; y < length; y++) {
+
+                        // Compares the elements in the array indexed in x and y
                         if (array[x].compareTo(array[y]) > 0) {
+
+                            // Swaps the array's element in index x with the element in index y
                             String temp = array[x];
                             array[x] = array[y];
                             array[y] = temp;
-                        }
-                    }
-                }
+                        } // End of if statement
+                    }// End of second for loop
+                } // End of first for loop
+                break;
 
             case 3: // insertion sort
                 for (int i = 1; i < length; ++i) {
