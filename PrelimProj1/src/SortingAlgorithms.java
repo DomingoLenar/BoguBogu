ecf9a34101ec3e99cbf04e3a3b74b91282abd474
Aaron Kyle Aguilar <2233692@slu.edu.ph>
2023-08-29 10:21:51 +0800

    + Modified the SortingAlgorithms.java + Added comments for the revised SortingAlgorithms.java

--- PrelimProj1/src/SortingAlgorithms.java
+++ PrelimProj1/src/SortingAlgorithms.java
@@ -1,22 +1,27 @@
 public class SortingAlgorithms {
+
+    // Sorts the input array using the specified sorting algorithm
+
     void sortingProcess(int sortingType, String[] array)
     {
         int length = array.length;
 
-        switch (sortingType)
-        {
-            case 1: // selection sort
+        switch (sortingType) {
+            case 1: // Selection Sort
                 for (int x = 0; x < length - 1; x++) {
                     int minIndex = x;
                     for (int y = x + 1; y < length; y++) {
-                        if (array[minIndex].compareTo(array[y]) > 0) minIndex = y;
+                        if (array[minIndex].compareTo(array[y]) > 0)
+                            minIndex = y;
                     }
                     if (minIndex != x) {
+                        // Swap elements
                         String temp = array[x];
                         array[x] = array[minIndex];
                         array[minIndex] = temp;
                     }
                 }
+                break; // Break to exit the case
 
             case 2: // bubble sort
                 for (int x = 0; x < length - 1; x++) {
@@ -28,8 +33,9 @@ public class SortingAlgorithms {
                         }
                     }
                 }
+                break; // Break to exit the case
 
-            case 3: // insertion sort
+            case 3: // Insertion Sort
                 for (int i = 1; i < length; ++i) {
                     String key = array[i];
                     int j = i - 1;
