9c15d524f55049e3d54a51376f30c4eaff99ac13
Lestat <2233915@slu.edu.ph>
2023-09-20 22:51:50 +0800

    +username and password fields

--- PrelimProj2/src/frontend/LogIn.java
+++ PrelimProj2/src/frontend/LogIn.java
@@ -5,4 +5,30 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class LogIn extends JLayeredPane{
+
+    JTextField username, password;
+    JButton signUp, forgetPass;
+
+
+    public LogIn(JFrame frame){
+        int width = frame.getWidth(), height = frame.getHeight();
+
+        setLayout(null);
+        setVisible(true);
+        setSize(600, 600);
+
+        username = new JTextField();
+        password.setBounds(4, 10, 60, 20);
+        username.setText("Username");
+
+        password = new JTextField();
+        password.setBounds(4, 40, 60, 20);
+        password.setText("Password");
+
+        add(username);
+        add(password);
+
+
+    }
+
 }
