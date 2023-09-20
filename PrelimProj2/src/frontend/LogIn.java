0ee1b73967dee3c925f54e19225630d98e5d7d9d
Lestat <2233915@slu.edu.ph>
2023-09-20 23:02:04 +0800

    +executable method, skeleton initial design

--- PrelimProj2/src/frontend/LogIn.java
+++ PrelimProj2/src/frontend/LogIn.java
@@ -9,26 +9,50 @@ public class LogIn extends JLayeredPane{
     JTextField username, password;
     JButton signUp, forgetPass;
 
+    JLabel header;
+
 
     public LogIn(JFrame frame){
         int width = frame.getWidth(), height = frame.getHeight();
 
         setLayout(null);
         setVisible(true);
-        setSize(600, 600);
+        setSize(width, height);
+
+        header = new JLabel();
+        header.setBounds(4,10, 50,50);
+        header.setText("Log In");
 
         username = new JTextField();
-        password.setBounds(4, 10, 60, 20);
+        username.setBounds(400, 10, 100, 20);
         username.setText("Username");
 
         password = new JTextField();
-        password.setBounds(4, 40, 60, 20);
+        password.setBounds(400, 50, 100, 20);
         password.setText("Password");
 
+        add(header);
         add(username);
         add(password);
 
+        validate();
+        repaint();
 
+
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setLayout(null);
+        frame.setVisible(true);
+        frame.setSize(600,600);
+        frame.setResizable(false);
+
+        LogIn panel = new LogIn(frame);
+        frame.add(panel);
+
+        frame.revalidate();
+        frame.repaint();
     }
 
 }
