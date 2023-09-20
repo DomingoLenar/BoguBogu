554df100ce238d27f58a379ac6e1bee355b3710d
BodaciousMuse <gebreyl2004@gmail.com>
2023-09-20 22:32:10 +0800

    + Added an interface for both linked lists

---
+++ PrelimProj2/src/tools/List.java
@@ -0,0 +1,16 @@
+/**
+ * September 20, 2023
+ */
+package tools;
+
+public interface List <T>{
+    void add();
+    void insert (T element, int index);
+    void deleteAt (int index);
+
+    void deleteAtHead();
+    void deleteAtTail();//
+    void clear();
+    boolean contains();
+    int size();
+}//End of interface
