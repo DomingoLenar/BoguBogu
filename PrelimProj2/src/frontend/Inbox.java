adc7cd08181ef551bc12908b37f9be2774ef6e84
Lestat <2233915@slu.edu.ph>
2023-09-20 23:15:49 +0800

    +ui variables

--- PrelimProj2/src/frontend/Inbox.java
+++ PrelimProj2/src/frontend/Inbox.java
@@ -6,11 +6,18 @@ import java.awt.event.ActionListener;
 
 public class Inbox extends JLayeredPane {
     // instantiate field variables
+    JList inboxList;
+    JButton settings, inboxMenu;
+
 
     public Inbox(JFrame frame){
         int width = frame.getWidth();
         int height = frame.getHeight();
+
+        setLayout(null);
         setVisible(true);
 
     }
+
+
 }
