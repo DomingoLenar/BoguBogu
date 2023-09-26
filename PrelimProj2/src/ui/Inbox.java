40ac8d3a2d4cf3c750347fa3ad67f227112fda27
Lou <2233672@slu.edu.ph>
2023-09-26 19:00:46 +0800

    +bound UI of Inbox.java & Login.java

--- PrelimProj2/src/ui/Inbox.java
+++ PrelimProj2/src/ui/Inbox.java
@@ -1,24 +1,130 @@
 package ui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 
 public class Inbox {
+    private final String mainPanelID = "main_ID";
+
+    private final String inboxID = "inbox_ID";
+
+    private final String sentID = "sent_ID";
+    private final String settingsID = "settings_ID";
     private JPanel mainPanel;
-    private JButton button3;
-    private JButton button1;
-    private JButton button2;
-    private JButton button4;
-    private JButton button5;
+    private JButton inboxButton;
+    private JButton sentButton;
+    private JButton settingsButton;
+    private JButton replyButton;
+    private JButton forwardButton;
+    private JPanel leftPanel;
+    private JPanel inboxPanel;
+    private JPanel contactsPanel;
+    private JPanel composeLetterPanel;
+    private JPanel cardPanel;
+    private JPanel letterPanel;
+    private JPanel attachImgPanel;
+    private JPanel attachFilePanel;
+    private JTextField searchField;
+    private JList list1;
+    private JPanel sentPanel;
+    private JPanel settingsPanel;
+
+    public JPanel getCardPanel() {
+        return cardPanel;
+    }
 
     public JPanel getMainPanel() {
         return mainPanel;
     }
 
+    public JPanel getSentPanel() {
+        return sentPanel;
+    }
+
     public Inbox()
     {
+        replyButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
+            }
+        });
+        forwardButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
+            }
+        });
+        inboxButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                // display inbox panel
+                displayInboxComponents();
+            }
+        });
 
+        sentButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                // display the sent panel
+                displaySentComponents();
+            }
+        });
+        settingsButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                // display the settings panel
+                displaySettingsComponents();
+            }
+        });
+        
+        setUpFrame();
+
+        // add the card panel in the main frame
+        mainPanel.add(cardPanel);
+
+        // show the main panel
+        ((CardLayout) cardPanel.getLayout()).show(cardPanel, inboxID);
+        
+    }
+
+    private void setUpFrame() {
+        cardPanel.add(inboxPanel, inboxID);
+        cardPanel.add(sentPanel, sentID);
+        cardPanel.add(settingsPanel, settingsID);
+    }
+    private void displaySettingsComponents() {
+        changeScreen(settingsID);
+    }
+    private void displayInboxComponents() {
+        changeScreen(inboxID);
+    }
+    private void displaySentComponents() {
+        changeScreen(sentID);
+    }
+
+    private void changeScreen(String screen) {
+        ((CardLayout)cardPanel.getLayout()).show(cardPanel, screen); // -> show the appropriate panel
+        // The arguments are the "parent panel" and the "id" of a child panel in a parent panel, respectively.
+    }
+
+    public JButton getInboxButton() {
+        return inboxButton;
+    }
+
+    public JButton getSentButton() {
+        return sentButton;
+    }
+
+    public JButton getSettingsButton() {
+        return settingsButton;
     }
 }
