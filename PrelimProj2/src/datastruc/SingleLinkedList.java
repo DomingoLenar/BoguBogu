e2eebccef4a31208af6f6833001f9da5d0690d31
BodaciousMuse <gebreyl2004@gmail.com>
2023-09-28 23:27:40 +0800

    + Added methods for the singly linked list

--- PrelimProj2/src/datastruc/SingleLinkedList.java
+++ PrelimProj2/src/datastruc/SingleLinkedList.java
@@ -1,5 +1,6 @@
 /**
- * September 15 | 20, 2023
+ * Class for the singly linked list
+ * September 15 | 20 | 27, 2023
  */
 package datastruc;
 
@@ -11,6 +12,8 @@ public class SingleLinkedList <T> implements List {
      */
     private SingleNode <T> head;
     private SingleNode <T> tail;
+
+    private int size;
 
     /**
      * Default constructor
@@ -32,69 +35,127 @@ public class SingleLinkedList <T> implements List {
     }
 
     /**
+     * Method to add an element
      * September 20, 2023
      */
     @Override
-    public void add() {
-
+    public void add(Object data) {
+        SingleNode newNode = new SingleNode(data);
+        if (this.head == null) {
+            head = newNode;
+            tail = head;
+            size++;
+        } else {
+            tail.setLink(newNode);
+            tail = newNode;
+            size++;
+        }
     }
 
     /**
-     * Adds an element at a specific index
-     * September 20, 2023
-     */
-    @Override
-    public void insert(Object element, int index) {
-
-    }
-
-    /**
-     * Deletes an element at a specific index
+     * Method to delete an element at a specific index
      * September 20, 2023
      */
     @Override
     public void deleteAt(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds.");
+        }
 
+        if (index == 0) {
+            head = head.getLink();
+            size--;
+
+            if (size == 0) {
+                tail = null;
+            }
+        } else {
+            SingleNode<T> current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.getLink();
+            }
+            current.setLink(current.getLink().getLink());
+
+            if (index == size - 1) {
+                tail = current;
+            }
+            size--;
+        }
     }
 
     /**
+     * Method to delete an element at the head
      * September 20, 2023
      */
     @Override
     public void deleteAtHead() {
-
+        if (head != null){
+            head.getLink();
+            size--;
+        }
+        if (head == null){
+            tail = null;
+        }
     }
 
     /**
+     * Method to delete an element at the tail
      * September 20, 2023
      */
     @Override
     public void deleteAtTail() {
+        if (head == null){
+            return;
+        }
+        if (head.getLink() == null){
+            head = null;
+            tail = null;
+            size--;
+            return;
+        }
 
+        SingleNode<T> current = head;
+        while (current.getLink().getLink() != null){
+            current = current.getLink();
+        }
+        current.setLink(null);
+        tail = current;
+        size--;
     }
 
 
     /**
+     * Method to clear the current list
      * September 20, 2023
      */
     @Override
     public void clear() {
-
+        head = null;
+        tail = null;
+        size = 0;
     }
 
     /**
+     * Method to return size of the linked list
+     * September 27, 2023
+     */
+    @Override
+    public int getSize(){
+        return this.size;
+    }
+
+    /**
+     * Method to check if a specified element is within the list
      * September 20, 2023
      */
     @Override
-    public boolean contains() {
+    public boolean contains(Object T) {
+        SingleNode<T> current = head;
+        while (current != null) {
+            if (current.getData().equals(T)) {
+                return true;
+            }
+        }
         return false;
     }
-
-    /**
-     * September 20, 2023
-     */
-    @Override
-    public int size() {
-        return 0;
-    }
-}//End of class
+}//end of class
