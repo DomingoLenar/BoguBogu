31ba47babb7316f4b0652fe7afc55b2cd8026db8
Aaron <2233692@slu.edu.ph>
2023-10-01 20:25:51 +0800

    +created comments for the DoubleLinkedListTest +created comments for the SingleLinkedListTest

--- PrelimProj2/src/testcases/DoubleLinkedListTest.java
+++ PrelimProj2/src/testcases/DoubleLinkedListTest.java
@@ -8,20 +8,24 @@ public class DoubleLinkedListTest {
 
     public DoubleLinkedList<Integer> list;
 
+    // This method runs before each test case to set up a fresh instance of SingleLinkedList
     @Before
     public void setUp() {
         list = new DoubleLinkedList<>();
     }
 
+    // Test case for the 'add' method
     @Test
     public void testAdd() {
         list.add(1);
         list.add(2);
         list.add(3);
 
+        // Check if the size of the list is as expected
         assertEquals(3, list.getSize());
     }
 
+    // Test case for the 'deleteAt' method
     @Test
     public void testDeleteAt() {
         list.add(1);
@@ -29,10 +33,13 @@ public class DoubleLinkedListTest {
         list.add(3);
 
         list.deleteAt(1);
+
+        // Check if the size is reduced and the deleted element is not in the list
         assertEquals(2, list.getSize());
         assertFalse(list.contains(2));
     }
 
+    // Test case for the 'deleteAtHead' method
     @Test
     public void testDeleteAtHead() {
         list.add(1);
@@ -40,10 +47,13 @@ public class DoubleLinkedListTest {
         list.add(3);
 
         list.deleteAtHead();
+
+        // Check if the size is reduced and the head element is removed
         assertEquals(2, list.getSize());
         assertFalse(list.contains(1));
     }
 
+    // Test case for the 'deleteAtTail' method
     @Test
     public void testDeleteAtTail() {
         list.add(1);
@@ -51,10 +61,13 @@ public class DoubleLinkedListTest {
         list.add(3);
 
         list.deleteAtTail();
+
+        // Check if the size is reduced and the tail element is removed
         assertEquals(2, list.getSize());
         assertFalse(list.contains(3));
     }
 
+    // Test case for the 'clear' method
     @Test
     public void testClear() {
         list.add(1);
@@ -62,16 +75,21 @@ public class DoubleLinkedListTest {
         list.add(3);
 
         list.clear();
+
+        // Check if the list is cleared and the size becomes 0
         assertEquals(0, list.getSize());
     }
 
+    // Test case for the 'contains' method
     @Test
     public void testContains() {
         list.add(1);
         list.add(2);
         list.add(3);
 
+
+        // Check if the 'contains' method correctly identifies elements in the list
         assertTrue(list.contains(2));
         assertFalse(list.contains(4));
     }
-}
+}  // End of DoubleLinkedListTest class
--- PrelimProj2/src/testcases/SingleLinkedListTest.java
+++ PrelimProj2/src/testcases/SingleLinkedListTest.java
@@ -8,20 +8,27 @@ public class SingleLinkedListTest {
 
     public SingleLinkedList<Integer> list;
 
+    // This method runs before each test case to set up a fresh instance of SingleLinkedList
     @Before
     public void setUp() {
         list = new SingleLinkedList<>();
     }
 
+
+    // Test case for the 'add' method
     @Test
     public void testAdd() {
         list.add(1);
         list.add(2);
         list.add(3);
 
+
+        // Check if the size of the list is as expected
         assertEquals(3, list.getSize());
     }
 
+
+    // Test case for the 'deleteAt' method
     @Test
     public void testDeleteAt() {
         list.add(1);
@@ -29,10 +36,14 @@ public class SingleLinkedListTest {
         list.add(3);
 
         list.deleteAt(1);
+
+        // Check if the size is reduced and the deleted element is not in the list
         assertEquals(2, list.getSize());
         assertFalse(list.contains(2));
     }
 
+
+    // Test case for the 'deleteAtHead' method
     @Test
     public void testDeleteAtHead() {
         list.add(1);
@@ -40,10 +51,14 @@ public class SingleLinkedListTest {
         list.add(3);
 
         list.deleteAtHead();
+
+        // Check if the size is reduced and the head element is removed
         assertEquals(2, list.getSize());
         assertFalse(list.contains(1));
     }
 
+
+    // Test case for the 'deleteAtTail' method
     @Test
     public void testDeleteAtTail() {
         list.add(1);
@@ -51,10 +66,13 @@ public class SingleLinkedListTest {
         list.add(3);
 
         list.deleteAtTail();
+
+        // Check if the size is reduced and the tail element is removed
         assertEquals(2, list.getSize());
         assertFalse(list.contains(3));
     }
 
+    // Test case for the 'clear' method
     @Test
     public void testClear() {
         list.add(1);
@@ -62,16 +80,22 @@ public class SingleLinkedListTest {
         list.add(3);
 
         list.clear();
+
+        // Check if the list is cleared and the size becomes 0
         assertEquals(0, list.getSize());
     }
 
+
+    // Test case for the 'contains' method
     @Test
     public void testContains() {
         list.add(1);
         list.add(2);
         list.add(3);
 
+
+        // Check if the 'contains' method correctly identifies elements in the list
         assertTrue(list.contains(2));
         assertFalse(list.contains(4));
     }
-}
+}  // End of SingleLinkedListTest class
