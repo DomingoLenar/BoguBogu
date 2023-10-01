ce47a0e25c34595552c1429cd41be0da716c1cbb
Aaron <2233692@slu.edu.ph>
2023-10-01 20:14:04 +0800

    +created partial test case for DoubleLinkedList +created partial test case for SingleLinkedList

--- PrelimProj2/src/testcases/DoubleLinkedListTest.java
+++ PrelimProj2/src/testcases/DoubleLinkedListTest.java
@@ -1,7 +1,77 @@
 package testcases;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
+import datastruc.DoubleLinkedList;
+import org.junit.Before;
+import org.junit.Test;
 
-class DoubleLinkedListTest {
+public class DoubleLinkedListTest {
 
+    public DoubleLinkedList<Integer> list;
+
+    @Before
+    public void setUp() {
+        list = new DoubleLinkedList<>();
+    }
+
+    @Test
+    public void testAdd() {
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        assertEquals(3, list.getSize());
+    }
+
+    @Test
+    public void testDeleteAt() {
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        list.deleteAt(1);
+        assertEquals(2, list.getSize());
+        assertFalse(list.contains(2));
+    }
+
+    @Test
+    public void testDeleteAtHead() {
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        list.deleteAtHead();
+        assertEquals(2, list.getSize());
+        assertFalse(list.contains(1));
+    }
+
+    @Test
+    public void testDeleteAtTail() {
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        list.deleteAtTail();
+        assertEquals(2, list.getSize());
+        assertFalse(list.contains(3));
+    }
+
+    @Test
+    public void testClear() {
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        list.clear();
+        assertEquals(0, list.getSize());
+    }
+
+    @Test
+    public void testContains() {
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        assertTrue(list.contains(2));
+        assertFalse(list.contains(4));
+    }
 }
