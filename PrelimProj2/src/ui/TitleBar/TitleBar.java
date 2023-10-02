acca4d214ebc8cd51ff15b789d5fcce72f81bd49
Lenar R. Domingo <2233894@slu.edu.ph>
2023-10-02 23:31:00 +0800

    + Title Bar

---
+++ PrelimProj2/src/ui/TitleBar/TitleBar.java
@@ -0,0 +1,30 @@
+package ui.TitleBar;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TitleBar extends JFrame {
+
+    public TitleBar() {
+        try {
+            // Set the icon for the JFrame (logo in the title bar)
+            ImageIcon logoIcon = new ImageIcon("PrelimProj2/src/resources/icons/Bogu Mail 1.jpg");
+            setIconImage(logoIcon.getImage());
+
+            // Set the title for the JFrame (text in the title bar)
+            setTitle("Bogu Bogu Mail");
+
+            // Set other frame properties
+            setSize(400, 300);
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            setLocationRelativeTo(null); // Center the frame
+            setVisible(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new TitleBar());
+    }
+}
