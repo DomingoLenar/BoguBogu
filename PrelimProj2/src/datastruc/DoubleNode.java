472084562e8bc9017129fd6a21c2838283d75058
RIZZA MONTEVIRGEN <2234110@slu.edu.ph>
2023-10-02 13:15:16 +0000

    Update DoubleNode.java documentation

--- PrelimProj2/src/datastruc/DoubleNode.java
+++ PrelimProj2/src/datastruc/DoubleNode.java
@@ -1,5 +1,10 @@
 /**
- * September 15 | 20, 2023
+ * A generic doubly linked list node class.
+ *
+ * This class represents a node containing data and links to the next and previous nodes.
+ *
+ * @param <T> The type of data stored in the node.
+ * @since September 15, 2023
  */
 package datastruc;
 
@@ -14,6 +19,7 @@ public class DoubleNode <T>{
 
     /**
      * Default Constructor
+     * Initializes a new DoubleNode with no data.
      */
     public DoubleNode (){
         this(null);
@@ -21,33 +27,64 @@ public class DoubleNode <T>{
 
     /**
      * Parameterized Constructor
+     * Initializes a new DoubleNode with the specified data.
+     *
+     * @param data The data to be stored in the node.
      */
     public DoubleNode (T data){
         this.data = data;
     }
 
     /**
-     * Getters
+     * Retrieves the data stored in the node.
+     *
+     * @return The data stored in the node.
      */
     public T getData() {
         return data;
-    }
+    } 
+
+    /**
+     * Retrieves the link to the next node.
+     *
+     * @return The link to the next node.
+     */
     public DoubleNode<T> getLink() {
         return link;
-    }
+    } 
+
+    /**
+     * Retrieves the link to the previous node.
+     *
+     * @return The link to the previous node.
+     */
     public DoubleNode<T> getLeftLink() {
         return leftLink;
     }
 
     /**
-     * Setters
+     * Sets the data to be stored in the node.
+     *
+     * @param data The data to be stored in the node.
      */
     public void setData(T data) {
         this.data = data;
     }
+
+    /**
+     * Sets the link to the next node.
+     *
+     * @param link The link to the next node.
+     */
     public void setLink(DoubleNode<T> link) {
         this.link = link;
     }
+
+    /**
+     * Sets the link to the previous node.
+     *
+     * @param leftLink The link to the previous node.
+     */
     public void setLeftLink(DoubleNode<T> leftLink) {
         this.leftLink = leftLink;
     }
