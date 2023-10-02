56e03503ba96363e7bb1379bf8801ca14390d5e5
RIZZA MONTEVIRGEN <2234110@slu.edu.ph>
2023-10-02 11:24:17 +0000

    Update FileHandling.java documentation

--- PrelimProj2/src/tools/FileHandling.java
+++ PrelimProj2/src/tools/FileHandling.java
@@ -7,10 +7,25 @@ import java.nio.file.Files;
 
 public class FileHandling {
 
+    /**
+     * Checks if a directory exists at the specified path.
+     *
+     * @param directory The path to the directory to check.
+     * @return True if the directory exists; otherwise, false.
+     */
+     
     public boolean directoryExists(String directory){
         Path path = Paths.get(directory);
         return Files.exists(path);
     }
+
+    /**
+     * Creates a new directory at the specified path.
+     *
+     * @param directory The path where the new directory should be created.
+     * @return True if the directory was successfully created; otherwise, false.
+     * @throws Exception If an error occurs during directory creation.
+     */
 
     public boolean createDirectory(String directory) throws Exception{
         Path path = Paths.get(directory);
