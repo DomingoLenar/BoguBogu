27272fd4f91e7214e0be89e64ba990ea68a9c50b
Lou <2233672@slu.edu.ph>
2023-10-20 22:48:06 +0800

    +Stackable interface

---
+++ MidtermProj1/src/Stackable.java
@@ -0,0 +1,10 @@
+public interface Stackable<T> {
+    int size();
+    void push(T element);
+    T pop() throws StackUnderflowException;
+    T peek() throws StackUnderflowException;
+    boolean isEmpty();
+    String stackToString(String delimiter) throws StackUnderflowException;
+
+}
+
