91199d7a486d357d2617138fe59e8788aa484cfb
Lou <2233672@slu.edu.ph>
2023-10-20 22:47:27 +0800

    +Node template class

---
+++ MidtermProj1/src/Node.java
@@ -0,0 +1,30 @@
+public class Node<T> {
+    Node<T> next;
+    T data;
+
+    public Node() {
+        setNext(null);
+        setData(null);
+    } // end of default constructor
+
+    public Node(T data) {
+        setNext(null);
+        setData(data);
+    } // end of constructor
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setNext(Node<T> node) {
+        next = node;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+} // end of Node class
