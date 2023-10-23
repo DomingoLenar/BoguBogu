3eaceb4be177e15e79a057a02956b90db8b3ba4e
Gebreyl Isaac Rabang <2233944@slu.edu.ph>
2023-10-23 21:06:59 +0800

    + Added documentation

--- MidProj1/src/PostfixEvaluator.java
+++ MidProj1/src/PostfixEvaluator.java
@@ -1,6 +1,19 @@
+/**
+ * Class for postfix expression evaluation
+ * October 23. 2023
+ */
+
 import java.util.*;
 
 public class PostfixEvaluator {
+
+    /**
+     * This method evaluates a given postfix expression by iterating through its tokens (operands and operators).
+     * It uses a stack to manage operands and performs arithmetic operations when operators are encountered.
+     * If the expression is valid and well-formed, it returns the final result.
+     * If the expression contains invalid tokens or doesn't have enough operands for
+       an operator, it throws exceptions to report errors.
+     */
     public static double evaluatePostfixExpression(String postfixExpression) {
         Stack<Double> operandStack = new Stack<>();
         String[] tokens = postfixExpression.split(" ");
@@ -32,7 +45,12 @@ public class PostfixEvaluator {
             throw new IllegalArgumentException("Invalid expression: Too many operands or operators.");
         }
         return operandStack.pop();
-    }
+    }//end of evaluatePostfixExpression
+
+    /**
+     *  This method checks whether a given token is a valid operand by attempting to convert it to a numeric value.
+     *  If the conversion is successful, the token is considered an operand; otherwise, it is not.
+     */
     private static boolean isOperand(String token) {
         try {
             Double.parseDouble(token);
@@ -40,10 +58,20 @@ public class PostfixEvaluator {
         } catch (NumberFormatException e) {
             return false;
         }
-    }
+    }//end of isOperand
+
+    /**
+     * This method checks whether a given token is a valid operator by comparing it to a predefined
+     list of supported arithmetic operators (e.g., "+", "-", "*", "/", "$").
+     * If the token matches one of these operators, it is considered a valid operator; otherwise, it is not.
+     */
     private static boolean isOperator(String token) {
         return "+".equals(token) || "-".equals(token) || "*".equals(token) || "/".equals(token) || "$".equals(token);
-    }
+    }//end of isOperator
+
+    /**
+     * This method takes an operator and two operands as input and applies the specified arithmetic operation.
+     */
     private static double applyOperator(String operator, double operand1, double operand2) {
         switch (operator) {
             case "+":
@@ -60,5 +88,5 @@ public class PostfixEvaluator {
             default:
                 throw new IllegalArgumentException("Unsupported operator: " + operator);
         }
-    }
-}
+    }//end of applyOperator
+}//end of class
