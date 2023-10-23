bc65d21da9a8b74dfe02361c08affeab06f038be
Gebreyl Isaac Rabang <2233944@slu.edu.ph>
2023-10-23 20:49:46 +0800

    + Added evaulatePostfixExpression method to evaluate a given postfix expression using a stack data structure

--- MidProj1/src/PostfixEvaluator.java
+++ MidProj1/src/PostfixEvaluator.java
@@ -1,7 +1,39 @@
 import java.util.*;
 
 public class PostfixEvaluator {
-        private static boolean isOperand(String token) {
+    public static double evaluatePostfixExpression(String postfixExpression) {
+        Stack<Double> operandStack = new Stack<>();
+        String[] tokens = postfixExpression.split(" ");
+
+        for (String token : tokens) {
+            if (isOperand(token)) {
+                operandStack.push(Double.parseDouble(token));
+            } else if (isOperator(token)) {
+                if (operandStack.size() < 2) {
+                    throw new IllegalArgumentException("Invalid expression: Not enough operands for operator.");
+                }
+                if ("$".equals(token)) {
+                    double operand2 = operandStack.pop();
+                    double operand1 = operandStack.pop();
+                    double result = Math.pow(operand1, operand2);
+                    operandStack.push(result);
+                } else {
+                    double operand2 = operandStack.pop();
+                    double operand1 = operandStack.pop();
+                    double result = applyOperator(token, operand1, operand2);
+                    operandStack.push(result);
+                }
+            } else {
+                throw new IllegalArgumentException("Invalid token: " + token);
+            }
+        }
+
+        if (operandStack.size() != 1) {
+            throw new IllegalArgumentException("Invalid expression: Too many operands or operators.");
+        }
+        return operandStack.pop();
+    }
+    private static boolean isOperand(String token) {
         try {
             Double.parseDouble(token);
             return true;
