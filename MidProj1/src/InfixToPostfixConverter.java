cc019f8370704aa78fe16535f9e8a5c56ff8268e
Carlo <2233085@slu.edu.ph>
2023-10-24 01:00:31 +0800

    + Added comments to InfixToPostfixConverter class.

--- MidProj1/src/InfixToPostfixConverter.java
+++ MidProj1/src/InfixToPostfixConverter.java
@@ -1,15 +1,18 @@
 import java.util.Stack;
 public class InfixToPostfixConverter {
+
+    // Create an instance of the Operators class to handle operator-related operations.
     private final Operators execute;
 
+    // Constructor for InfixToPostfixConverter class.
     public InfixToPostfixConverter() {
-        execute = new Operators();
+        execute = new Operators(); // Initialize the 'execute' field with a new instance of the Operators class.
     }
 
     /*
-     #TODO: method enhance implementation is possible to hold all possible cases
-     * Problem: it does not give correct postfix when user input an infix that is factorized/simplified form (i.e., a-b(c+d))
-     * Possible solution: use helper method to transform simplified infix to general infix expression or construct entirely new method that is capable converting any infix to postfix expression
+     * Converts an infix expression to a postfix expression.
+     * This method takes an infix expression in the form of a space-separated string
+     * and converts it into a postfix expression using a stack-based algorithm.
      */
     public String convert(String infixString) throws StackUnderflowException {
         StringBuilder postfixExpression = new StringBuilder();
@@ -31,17 +34,17 @@ public class InfixToPostfixConverter {
                         postfixExpression.append(operatorStack.pop());
                     }
                     operatorStack.pop();
-                    while (!operatorStack.isEmpty()){ // get remaining operators (handle infix w parenthesis)
+                    while (!operatorStack.isEmpty()){ // Get the remaining operators (handling infix with parenthesis).
                         postfixExpression.append(operatorStack.pop());
                     }
                 }
             }
         }
 
-        while (!operatorStack.isEmpty()){ // get rest operators in stack (infix w/o parenthesis)
+        while (!operatorStack.isEmpty()){ // Get the remaining operators in the stack (infix without parenthesis).
             postfixExpression.append(operatorStack.pop());
         }
 
         return postfixExpression.toString();
-    }
-}
+    } // End of convert method
+} // End of class
