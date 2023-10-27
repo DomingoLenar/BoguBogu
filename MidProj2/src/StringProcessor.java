7d73b04b618bf19a05254d1a051712c0e1798afb
Lestat <2233915@slu.edu.ph>
2023-10-27 10:24:06 +0800

    +custom node for linkedlist usage inside the character counter

--- MidProj2/src/StringProcessor.java
+++ MidProj2/src/StringProcessor.java
@@ -3,20 +3,18 @@ import java.util.*;
 public class StringProcessor {
     public String[][] getFrequency(String text){
         int length = text.length();
-        ArrayList<String[]> frequency = new ArrayList<>();
-        String[] toAdd = new String[2];
+        LinkedList<customNode> characterFrequency = new LinkedList<>();
         for(int x = 0; x < length; x++){
-            if(frequency.isEmpty()){
-                toAdd[0] = String.valueOf(text.charAt(x));
-                toAdd[1]
-                frequency.add();
+                if(characterFrequency.isEmpty()){
+                    customNode newNode = new customNode(String.valueOf(text.charAt(x)), 1);
+                    characterFrequency.add(newNode);
             }
         }
 
         return frequency;
     }
 
-    public int search(String searchKey, ArrayList<String[]> arrayList){
+    public int search(String searchKey, LinkedList<customNode> linkedList){
        int outerSize = arrayList.size();
        for(int x = 0; x < outerSize; x++){
            if(arrayList.get(x)[0].equals(searchKey)){
@@ -24,5 +22,29 @@ public class StringProcessor {
            }
        }
        return -1;
+    }
+}
+
+class customNode{
+    private String character;
+    private int frequency;
+    public customNode(String c, int f){
+        character = c;
+        frequency = f;
+    }
+    public String getCharac(){
+        return character;
+    }
+
+    public void setCharac(String c){
+        character = c;
+    }
+
+    public int getFrequency(){
+        return frequency;
+    }
+
+    public void setFrequency(int f){
+        frequency = f;
     }
 }
