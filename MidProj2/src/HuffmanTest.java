55e00dfea47d9a073c0f08be0d144cfb5f73da51
Lou <2233672@slu.edu.ph>
2023-11-07 17:43:32 +0800

    +fix huffman to string representation

--- MidProj2/src/HuffmanTest.java
+++ MidProj2/src/HuffmanTest.java
@@ -1,11 +1,14 @@
 
+
 import java.util.*;
 
 public class HuffmanTest implements Runnable{
     PriorityQueue<TreeNode> huffmanTree = new PriorityQueue<>();
     HashMap<Character, Integer> characters_no_bits = new HashMap<>();
     HashMap<Character, String> characters_huffman_code = new HashMap<>();
-    String huffmanCode = "";
+    String text_to_huffman = "";
+    TreeNode root = null;
+
     StringProcessor processor = new StringProcessor();
     Scanner kyb = new Scanner(System.in);
 
@@ -15,11 +18,11 @@ public class HuffmanTest implements Runnable{
      * @param s
      */
     public void huffmanCode(TreeNode root, String s){
-        if (root.getLeft() == null && root.getRight() == null){
+        if (root.getLeft() == null && root.getRight() == null && Character.isLetter(root.symbol)){
             System.out.println(root.getSymbol() + " | " + s + " | " + s.length());
             characters_no_bits.put(root.getSymbol(), s.length());
             characters_huffman_code.put(root.getSymbol(), s);
-            huffmanCode += s;
+            text_to_huffman += s;
             return;
         }
 
@@ -84,7 +87,6 @@ public class HuffmanTest implements Runnable{
              letter_frequency = processor.getFrequency(user_input_string);
             createTreeSkeleton(letter_frequency, huffmanTree);
 
-            TreeNode root = null;
 
             while (huffmanTree.size() > 1) { // construct the complete binary tree (huffman tree) to obtain huffman coding scheme
                 TreeNode t = huffmanTree.peek();
@@ -115,54 +117,68 @@ public class HuffmanTest implements Runnable{
         }
 
         String user_input_code = promptMessage2();
-        System.out.println(huffmanCode);
+        System.out.println(text_to_huffman);
 
         if (user_input_code == null)
             System.exit(0);
             // #TODO: add prompt message
         else if (user_input_code.matches("^[0-9]+$")) {
-            String s = "";
+//            String letter_huffman_code = "";
+//            letter_huffman_code = huffmanCode_text_representation(letter_huffman_code, user_input_code, root);
+
+            String huffman_to_text = "";
             int i = 0;
+
             while (!letter_frequency.isEmpty()) {
                 if ( i > letter_frequency.size() - 1) {
                     i = 0;
                 } else {
                     CustomNode customNode = letter_frequency.get(i);
                     String character_code = characters_huffman_code.get(customNode.getCharac().charAt(0));
-
                     if (user_input_code.isEmpty())
                         break;
 
                     if (user_input_code.contains(character_code) && user_input_code.startsWith(character_code)) {
-                        user_input_code = user_input_code.substring(character_code.length());
-                        s += customNode.getCharac();
+
+                        for (int j = 0; j < user_input_string.length(); j++) {
+                            if ( customNode.getCharac().charAt(0) == user_input_string.charAt(j)) {
+                                user_input_code = user_input_code.substring(character_code.length());
+                                huffman_to_text += customNode.getCharac();
+                                break;
+                            }
+                        }
                     }
                     i++;
                 }
-//                pattern = Pattern.compile(character_code);
-//                matcher = pattern.matcher(user_input_code);
-//
-//                if (!matcher.find()) {
-//                    System.out.println("ERROR");
-//                } else {
-//                    Matcher matcher1 = pattern.matcher(huffmanCode);
-//                    while (matcher1.find()) {
-//                        s = customNode.getCharacter();
-//                    }
-//                }
-
             }
-// 000100101
-// HAAHAABAAACACAAAABABABEBEBBAABEEBBCCCCCAAAAAAACCCDDDDDDAAADDDDDEEEEEEDDADDDADDEEEEEEEECCCEEEEEEEEEEDDEEEEEEEEEEBBEEEEEFFGGHAHHEEHHA
+            System.out.println(huffman_to_text);
 
-            System.out.println(s);
-        } else {
+//// 000100101
+//// HAAHAABAAACACAAAABABABEBEBBAABEEBBCCCCCAAAAAAACCCDDDDDDAAADDDDDEEEEEEDDADDDADDEEEEEEEECCCEEEEEEEEEEDDEEEEEEEEEEBBEEEEEFFGGHAHHEEHHA
+        }
+        else {
             // #TODO: add prompt message
             System.out.println("ERROR");
             System.exit(0);
         }
 
     }
+
+//    private String huffmanCode_text_representation(String letterHuffmanCode, String user_input_code, TreeNode root) {
+//
+//            if (root.getLeft() == null && root.getRight() == null) {
+//                letterHuffmanCode += characters_huffman_code.get(root.getSymbol());
+//
+//                if (letterHuffmanCode.startsWith(user_input_code)) {
+//                    user_input_code = user_input_code.substring(letterHuffmanCode.length() - 1);
+//                }
+//        }
+//            huffmanCode_text_representation(letterHuffmanCode, user_input_code, root.getLeft());
+//            huffmanCode_text_representation(letterHuffmanCode, user_input_code, root.getRight());
+//
+//            return letterHuffmanCode;
+//    }
+
     public String promptMessage1() {
         try {
             System.out.println("Input a string: ");
