5bcc5339c43ba593ae2756704ac4276f432edc2d
RIZZA MONTEVIRGEN <2234110@slu.edu.ph>
2023-11-10 11:11:57 +0000

    Added comments and documentation for CustomNode

--- MidProj2/src/StringProcessor.java
+++ MidProj2/src/StringProcessor.java
@@ -64,44 +64,95 @@ public class StringProcessor {
 
 }
 
+/**
+ * Represents a node in a custom linked list used for storing characters and their frequencies.
+ * Each node contains a character, a frequency, and a reference to the next node in the list.
+ */
 class CustomNode{
-    private String character;
-    private int frequency;
-    private CustomNode nextNode;
+    private String character;  // The character associated with this node
+    private int frequency; // The frequency of the character in the context of the application
+    private CustomNode nextNode; // Reference to the next node in the linked list
+
+    /**
+     * Parameterized constructor to create a CustomNode with specified character and frequency.
+     *
+     * @param character The character associated with this node.
+     * @param frequency The frequency of the character.
+     */
     public CustomNode(String c, int f){
         character = c;
         frequency = f;
         nextNode = null;
     }
+
+    /**
+     * Default constructor initializes the node with null character, zero frequency, and null next node.
+     */
     public CustomNode(){
         character = null;
         frequency = 0;
         nextNode = null;
     }
+    
+    /**
+     * Get the character associated with this node.
+     *
+     * @return The character.
+     */
     public String getCharac(){
         return character;
     }
 
+    /**
+     * Set the character associated with this node.
+     *
+     * @param character The character to be associated with this node.
+     */
     public void setCharac(String c){
         character = c;
     }
 
+    /**
+     * Get the next node in the linked list.
+     *
+     * @return Reference to the next node.
+     */
     public CustomNode getNextNode() {
         return nextNode;
     }
 
+    /**
+     * Set the next node in the linked list.
+     *
+     * @param nextNode Reference to the next node.
+     */
     public void setNextNode(CustomNode nextNode) {
         this.nextNode = nextNode;
     }
 
+    /**
+     * Get the frequency of the character associated with this node.
+     *
+     * @return The frequency.
+     */
     public int getFrequency(){
         return frequency;
     }
 
+    /**
+     * Set the frequency of the character associated with this node.
+     *
+     * @param frequency The frequency to be set.
+     */
     public void setFrequency(int f){
         frequency = f;
     }
 
+    /**
+     * Get a string representation of the character and frequency of this node.
+     *
+     * @return A string containing the character and frequency.
+     */
     public String toString(){
         return character+","+frequency;
     }
